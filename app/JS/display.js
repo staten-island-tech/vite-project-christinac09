c094abc1d1ad897fb65a7cd23fdaaa0fb5805b44
christina <158211764+christinac09@users.noreply.github.com>
2024-11-12 13:56:09 -0500

    sort alphabetically

--- app/JS/display.js
+++ app/JS/display.js
@@ -2,77 +2,95 @@ import { DOMSelectors } from "./dom";
 import { songs } from "./products";
 
 function addSongCards(products) {
-    products.forEach((product) =>
-      DOMSelectors.container.insertAdjacentHTML(
-        "beforeend",
-        `<div class="card">
+  products.forEach((product) =>
+    DOMSelectors.container.insertAdjacentHTML(
+      "beforeend",
+      `<div class="card">
           <h3 class="card-title">${product.title}</h3>
           <h4 class="card-artist">${product.artist}</h4>
           <p class="card-info">${product.releaseDate}</p>
           <img class="card-img" src="${product.imageUrl}" alt="${product.altText}">
           <h5 class="card-price">$${product.price}</h5>
         </div>`
-      )
-    );
-  }
+    )
+  );
+}
 
 function removeAllCards() {
   DOMSelectors.container.replaceChildren(); //removes all children
 }
 
+function getFilteredABC() {
+  let filtered;
+  removeAllCards();
+  filtered = songs.sort();
+  return filtered;
+}
+
 function getFilteredGenre(type) {
   let filtered;
-  let acceptedTypes = ["Pop", "K-pop", "KRnB", "Hip Hop"]
+  let acceptedTypes = ["Pop", "K-pop", "KRnB", "Hip Hop"];
   removeAllCards();
   if (type === "reset") {
-    filtered = songs
+    filtered = songs;
   } else if (type === "Pop") {
-    filtered = songs.filter((song) => song.genre === acceptedTypes[0])
+    filtered = songs.filter((song) => song.genre === acceptedTypes[0]);
   } else if (type === "K-pop") {
-    filtered = songs.filter((song)=> song.genre === acceptedTypes[1])
+    filtered = songs.filter((song) => song.genre === acceptedTypes[1]);
   } else if (type === "K-RnB") {
-    filtered = songs.filter((song)=> song.genre === acceptedTypes[2])
+    filtered = songs.filter((song) => song.genre === acceptedTypes[2]);
   } else if (type === "Hip Hop") {
-    filtered = songs.filter((song)=> song.genre === acceptedTypes[3])
+    filtered = songs.filter((song) => song.genre === acceptedTypes[3]);
   } else if (type === "Other") {
-    filtered = songs.filter((song)=> !acceptedTypes.includes(song.genre))
+    filtered = songs.filter((song) => !acceptedTypes.includes(song.genre));
   }
-  return filtered
+  return filtered;
 }
 
 function getFilteredRelease(type) {
   let filtered;
   removeAllCards();
   if (type === "2010-2015") {
-    filtered = songs.filter((song) => song.releaseYear <= 2015)
+    filtered = songs.filter((song) => song.releaseYear <= 2015);
   } else if (type === "2016-2020") {
-    filtered = songs.filter((song)=> song.releaseYear>=2016 && song.releaseYear<=2020)
+    filtered = songs.filter(
+      (song) => song.releaseYear >= 2016 && song.releaseYear <= 2020
+    );
   } else {
-    filtered = songs.filter((song)=>song.releaseYear >=2021)
+    filtered = songs.filter((song) => song.releaseYear >= 2021);
   }
-  return filtered
+  return filtered;
 }
 
 function filterButtons() {
   let type, filtered;
-  DOMSelectors.showAllButton.addEventListener("click", function(event) {
+  DOMSelectors.showAllButton.addEventListener("click", function (event) {
     event.preventDefault;
-    filtered = getFilteredGenre("reset")
-    addSongCards(filtered)
-  })
-  DOMSelectors.genreOptionButtons.forEach((genreOption)=>genreOption.addEventListener("click", function(event) {
+    filtered = getFilteredGenre("reset");
+    addSongCards(filtered);
+  });
+  DOMSelectors.alphabeticalButton.addEventListener("click", function (event) {
     event.preventDefault;
-    type = genreOption.innerHTML
-    filtered = getFilteredGenre(type)
-    addSongCards(filtered)
-  }))
-  DOMSelectors.releaseOptionButtons.forEach((releaseOption)=>releaseOption.addEventListener("click", function(event) {
-    event.preventDefault;
-    type = releaseOption.innerHTML
-    filtered = getFilteredRelease(type)
-    console.log(filtered)
-    addSongCards(filtered)
-  }))
+    filtered = getFilteredABC();
+    addSongCards(filtered);
+  });
+  DOMSelectors.genreOptionButtons.forEach((genreOption) =>
+    genreOption.addEventListener("click", function (event) {
+      event.preventDefault;
+      type = genreOption.innerHTML;
+      filtered = getFilteredGenre(type);
+      addSongCards(filtered);
+    })
+  );
+  DOMSelectors.releaseOptionButtons.forEach((releaseOption) =>
+    releaseOption.addEventListener("click", function (event) {
+      event.preventDefault;
+      type = releaseOption.innerHTML;
+      filtered = getFilteredRelease(type);
+      console.log(filtered);
+      addSongCards(filtered);
+    })
+  );
 }
 
-export {addSongCards, filterButtons}
+export { addSongCards, filterButtons };
--- app/JS/dom.js
+++ app/JS/dom.js
@@ -1,13 +1,14 @@
 const DOMSelectors = {
-    container: document.querySelector(".container"),
+  container: document.querySelector(".container"),
 
-    warmButton: document.getElementById("warm-btn"),
-    coolButton: document.getElementById("cool-btn"),
-    lightWarmButton: document.getElementById("lightwarm-btn"),
+  warmButton: document.getElementById("warm-btn"),
+  coolButton: document.getElementById("cool-btn"),
+  lightWarmButton: document.getElementById("lightwarm-btn"),
 
-    showAllButton: document.getElementById("reset-filter"),
-    genreOptionButtons: document.querySelectorAll(".genre-option"),
-    releaseOptionButtons: document.querySelectorAll(".release-option"),
-  };
+  showAllButton: document.getElementById("reset-filter"),
+  alphabeticalButton: document.getElementById("alphabetical-filter"),
+  genreOptionButtons: document.querySelectorAll(".genre-option"),
+  releaseOptionButtons: document.querySelectorAll(".release-option"),
+};
 
-export {DOMSelectors}
+export { DOMSelectors };
